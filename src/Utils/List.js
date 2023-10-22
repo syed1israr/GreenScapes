f9d6e12628f723c778233aae66c25051044dcaeb
Israr <syedfaraz173@gmail.com>
2023-10-23 01:43:02 +0530

    Second Commit

---
+++ src/Utils/List.js
@@ -0,0 +1,32 @@
+export const restaurants = [
+    {
+        id: "1",
+        dishName: "Veg Biryani",
+        description: "A delightful and aromatic dish made with fragrant basmati rice, assorted vegetables, and a blend of spices. It's a popular Indian one-pot meal that's both flavorful and satisfying.",
+    },
+    {
+        id: "2",
+        dishName: "Paneer Tikka",
+        description: "Paneer tikka is a popular Indian appetizer made by marinating paneer (cottage cheese) in a mixture of yogurt and spices, then grilling it until it's perfectly charred and smoky.",
+    },
+    {
+        id: "3",
+        dishName: "Mutton Curry",
+        description: "Mutton curry is a rich and spicy dish prepared with tender pieces of mutton cooked in a flavorful gravy made from aromatic spices, onions, and tomatoes. It's a classic Indian curry known for its bold flavors.",
+    },
+    {
+        id: "4",
+        dishName: "Kerala Sadya",
+        description: "Kerala Sadya is a traditional vegetarian feast from the South Indian state of Kerala. It features a variety of dishes, including rice, sambar, aviyal, thoran, and more, served on a banana leaf.",
+    },
+    {
+        id: "5",
+        dishName: "Greek Salad",
+        description: "A Greek salad is a refreshing and healthy dish made with fresh cucumbers, tomatoes, red onions, Kalamata olives, feta cheese, and a dressing of olive oil and herbs. It's a popular Mediterranean salad.",
+    },
+    {
+        id: "6",
+        dishName: "Pasta Carbonara",
+        description: "Pasta carbonara is an Italian pasta dish known for its creamy sauce made from eggs, cheese, pancetta, and black pepper. It's a classic Roman dish that's simple yet incredibly satisfying.",
+    },
+];
