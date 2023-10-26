953708d6570c0d4da7bf7fa4b97617ab96d59bfd
Israr <syedfaraz173@gmail.com>
2023-10-27 00:37:35 +0530

    fixed screen glitch

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -11,29 +11,29 @@ const Footer = () => {
            <div class="footer-col">
              <h4>company</h4>
              <ul>
-               <li><a >about us</a></li>
-               <li><a>our services</a></li>
-               <li><a>privacy policy</a></li>
-               <li><a>affiliate program</a></li>
+               <li><a href='https://practice.geeksforgeeks.org/batch/fsrnl-38?tab=Chapters' >about us</a></li>
+               <li><a href='https://practice.geeksforgeeks.org/batch/fsrnl-38?tab=Chapters'>our services</a></li>
+               <li><a href='https://practice.geeksforgeeks.org/batch/fsrnl-38?tab=Chapters'>privacy policy</a></li>
+               <li><a href='https://practice.geeksforgeeks.org/batch/fsrnl-38?tab=Chapters'>affiliate program</a></li>
              </ul>
            </div>
            <div class="footer-col">
              <h4>get help</h4>
              <ul>
-               <li><a>FAQ</a></li>
-               <li><a>shipping</a></li>
-               <li><a>returns</a></li>
-               <li><a>order status</a></li>
-               <li><a>payment options</a></li>
+               <li><a href='https://practice.geeksforgeeks.org/batch/fsrnl-38?tab=Chapters'>FAQ</a></li>
+               <li><a href='https://practice.geeksforgeeks.org/batch/fsrnl-38?tab=Chapters'>shipping</a></li>
+               <li><a href='https://practice.geeksforgeeks.org/batch/fsrnl-38?tab=Chapters'>returns</a></li>
+               <li><a href='https://practice.geeksforgeeks.org/batch/fsrnl-38?tab=Chapters'>order status</a></li>
+               <li><a href='https://practice.geeksforgeeks.org/batch/fsrnl-38?tab=Chapters'>payment options</a></li>
              </ul>
            </div>
            <div class="footer-col">
              <h4>online shop</h4>
              <ul>
-               <li><a>Rate Us</a></li>
-               <li><a>Franchise</a></li>
-               <li><a>location</a></li>
-               <li><a>Recipe Books</a></li>
+               <li><a href='https://practice.geeksforgeeks.org/batch/fsrnl-38?tab=Chapters'>Rate Us</a></li>
+               <li><a href='https://practice.geeksforgeeks.org/batch/fsrnl-38?tab=Chapters'>Franchise</a></li>
+               <li><a href='https://practice.geeksforgeeks.org/batch/fsrnl-38?tab=Chapters'>location</a></li>
+               <li><a href='https://practice.geeksforgeeks.org/batch/fsrnl-38?tab=Chapters'>Recipe Books</a></li>
              </ul>
            </div>
          </div>
