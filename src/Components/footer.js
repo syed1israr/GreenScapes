63583092c3087fe473925465eec080b95e234f2d
Israr <syedfaraz173@gmail.com>
2023-10-27 00:05:44 +0530

    Final Commit :(

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -5,25 +5,40 @@ import './App.css'
 const Footer = () => {
 
     return (
-        <footer className="footer">
-            <div className="container">
-                <div className="footer-content">
-                    <div className="footer-logo">
-                        <h1>Taste the flavors of the world!</h1>
-              
-                    </div>
-                   
-                    <div className="footer-contact">
-                        <p>Thanks For Visiting Out Site!</p>
-                        <address>
-                            <p1>123 Main Street</p1>
-                            <p1>City, State ZIP</p1>
-                        </address>
-                        <p>Call us: (123) 456-7890</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <footer class="footer">
+        <div class="container">
+         <div class="row">
+           <div class="footer-col">
+             <h4>company</h4>
+             <ul>
+               <li><a href="#">about us</a></li>
+               <li><a href="#">our services</a></li>
+               <li><a href="#">privacy policy</a></li>
+               <li><a href="#">affiliate program</a></li>
+             </ul>
+           </div>
+           <div class="footer-col">
+             <h4>get help</h4>
+             <ul>
+               <li><a href="#">FAQ</a></li>
+               <li><a href="#">shipping</a></li>
+               <li><a href="#">returns</a></li>
+               <li><a href="#">order status</a></li>
+               <li><a href="#">payment options</a></li>
+             </ul>
+           </div>
+           <div class="footer-col">
+             <h4>online shop</h4>
+             <ul>
+               <li><a href="#">Rate Us</a></li>
+               <li><a href="#">Franchise</a></li>
+               <li><a href="#">location</a></li>
+               <li><a href="#">Recipe Books</a></li>
+             </ul>
+           </div>
+         </div>
+        </div>
+     </footer>
     );
 };
 
