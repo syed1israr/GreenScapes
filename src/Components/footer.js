56b40341defb6e53744c466dbdc8a94d9dce38aa
Israr <syedfaraz173@gmail.com>
2023-10-27 00:29:46 +0530

    Final Commit ;(

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -11,29 +11,29 @@ const Footer = () => {
            <div class="footer-col">
              <h4>company</h4>
              <ul>
-               <li><a href="#">about us</a></li>
-               <li><a href="#">our services</a></li>
-               <li><a href="#">privacy policy</a></li>
-               <li><a href="#">affiliate program</a></li>
+               <li><a >about us</a></li>
+               <li><a>our services</a></li>
+               <li><a>privacy policy</a></li>
+               <li><a>affiliate program</a></li>
              </ul>
            </div>
            <div class="footer-col">
              <h4>get help</h4>
              <ul>
-               <li><a href="#">FAQ</a></li>
-               <li><a href="#">shipping</a></li>
-               <li><a href="#">returns</a></li>
-               <li><a href="#">order status</a></li>
-               <li><a href="#">payment options</a></li>
+               <li><a>FAQ</a></li>
+               <li><a>shipping</a></li>
+               <li><a>returns</a></li>
+               <li><a>order status</a></li>
+               <li><a>payment options</a></li>
              </ul>
            </div>
            <div class="footer-col">
              <h4>online shop</h4>
              <ul>
-               <li><a href="#">Rate Us</a></li>
-               <li><a href="#">Franchise</a></li>
-               <li><a href="#">location</a></li>
-               <li><a href="#">Recipe Books</a></li>
+               <li><a>Rate Us</a></li>
+               <li><a>Franchise</a></li>
+               <li><a>location</a></li>
+               <li><a>Recipe Books</a></li>
              </ul>
            </div>
          </div>
