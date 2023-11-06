9a806a43671eac18f143942c50aae29fdd4aa2c2
Israr <syedfaraz173@gmail.com>
2023-11-07 00:30:50 +0530

    Routing Nahi ho raha hai to code restore krke push kr raha hun  :(

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,40 +1,55 @@
-import '././App.css'
+import './App.css';
 import Card from './card';
- import { restaurants } from '../Utils/List'; 
+import { restaurants } from '../Utils/List';
 import { useState } from 'react';
 
-//hooks are Js functions
 function Body2() {
-  const [Searchtext,setSearchtext]=useState("");
-  const [filterdres,setfilterdres]=useState(restaurants);
-function filterres(){
- const filterdData=   restaurants.filter((restaurants)=>restaurants.dishName.toLowerCase().includes(Searchtext.toLowerCase()));
- setfilterdres(filterdData);
- console.log(filterdData)
-}
-function toprated(){
-  const topres=restaurants.filter((restaurants)=>restaurants.review>4)
-  setfilterdres(topres)
-}
+  const [searchText, setSearchText] = useState('');
+  const [filteredRes, setFilteredRes] = useState(restaurants);
+
+  function filterRes() {
+    const filteredData = restaurants.filter((restaurant) =>
+      restaurant.dishName.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredRes(filteredData);
+  }
+
+  function topRated() {
+    const topRes = restaurants.filter((restaurant) => restaurant.review > 4);
+    setFilteredRes(topRes);
+  }
+
   return (
-    <div className='body2' >
-      <input className='search' type="text" placeholder="Search Your Favorite Dish!" 
-      onChange={(e)=> setSearchtext(e.target.value)}/>
-      <button className='btn1'
-      onClick={filterres}>Search</button>
-      <button className='btn1' onClick={toprated}>Top 4⭐</button>
+    <div className='body2'>
+      <input
+        className='search'
+        type="text"
+        placeholder="Search Your Favorite Dish!"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <button className='btn1' onClick={filterRes}>
+        Search
+      </button>
+      <button className='btn1' onClick={topRated}>
+        Top 4⭐
+      </button>
       <div className='Menu_tag'>
-        {filterdres.map((restaurant) => (
-          <Card  
-            key={restaurant.id} 
-            name={restaurant.dishName}
-            description={restaurant.description}
-            image={restaurant.image}
-            review={restaurant.review}
-            price={restaurant.price}
-            time={restaurant.time}
-          />
-        ))}
+        {filteredRes.length > 0 ? (
+          filteredRes.map((restaurant) => (
+            <Card
+              key={restaurant.id}
+              name={restaurant.dishName}
+              description={restaurant.description}
+              image={restaurant.image}
+              review={restaurant.review}
+              price={restaurant.price}
+              time={restaurant.time}
+            />
+          ))
+        ) : (
+          <h4>No results found</h4>
+        )}
       </div>
     </div>
   );
