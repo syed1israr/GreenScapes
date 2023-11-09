a604bcbcdeb1d069d5c242eb9ccecc8b7a82d6a1
syedFaraz <syedfaraz173@gmail.com>
2023-11-10 00:05:57 +0530

    --

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <div className='Header'>
-      <img src={logo} className='logo' alt='Logo' />
+     <Link to="/" ><img src={logo} className='logo' alt='Logo' /></Link> 
       <ul>
-        <li><Link to="/">HomePage</Link></li>
+        <li> <Link to="https://github.com/syed1israr/react_small_project.git">Source-Code </Link></li>
         <li><Link to="/About">About</Link></li>
         <li>
           <Link to="/contact">Contact Us</Link>
