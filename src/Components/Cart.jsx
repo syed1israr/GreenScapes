755805fe5ca93f1cd5745eeca9ae9bf4dc6a8262
syedFaraz <syedfaraz173@gmail.com>
2023-11-10 06:21:15 +0530

    added some styles

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import background from '../Utils/Waiting.png'
-
+import React, { useEffect, useRef } from 'react';
+import background from '../Utils/Waiting.png';
+import gsap from 'gsap';
 
 function Cart() {
+  const imageAnim = useRef(null);
+
+  useEffect(() => {
+    gsap.to(imageAnim.current, 0.5, {
+      y: 30,
+      opacity: 1,
+      delay: 0.5,
+    },);
+  }, [imageAnim]);
+
   return (
-    <div>
-        <img src={background} alt="" srcset=""  className='UnderConstruction_cart_message'/>
+    <div className='bla'>
+      <img src={background} alt="" srcset="" className='UnderConstruction_cart_message' ref={imageAnim} />
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
