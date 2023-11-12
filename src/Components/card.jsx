83ec182857168eee9388cd0acb3bc33216a1efe4
syedFaraz <syedfaraz173@gmail.com>
2023-11-12 16:45:52 +0530

    Code not working properly

--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { addItem, clearCart } from '../Utils/Cart_Slice';
 import { TweenMax } from 'gsap/gsap-core';
@@ -6,37 +6,33 @@ import { TweenMax } from 'gsap/gsap-core';
 const Card = ({ name, description, image, review, price, time }) => {
   const [buttonText, setButtonText] = useState('Add To Cart');
   const dispatch = useDispatch();
-
   const handleAddItem = (event) => {
     dispatch(addItem({ name, price }));
     setButtonText('Added to Cart');
-
-    const button = event.target;
-    button.style.backgroundColor = 'white';
-    button.style.color = 'green';
+    event.target.style.backgroundColor = 'white';
+    event.target.style.color = 'green';
   };
-
+  
   const handleClearCart = (event) => {
     dispatch(clearCart());
     alert('Cart is Emptied :/');
-    setButtonText('Add To Cart');
-
-    const buttons = document.querySelectorAll('.adder');
-    for (const button of buttons) {
-      button.classList.remove('added-to-cart');
-      button.style.backgroundColor = '#20553A';
-      button.style.color = 'wheat';
-    }
+    setButtonText('Add To Cart'); 
+    const buttons = document.querySelectorAll('.adder'); 
+    buttons.forEach(button => button.classList.remove('added-to-cart')); 
+    event.target.style.backgroundColor = 'rgb(32, 85, 58)';
+    event.target.style.color = 'wheat';
   };
+  
+  
 
   useEffect(() => {
-    const cards = document.querySelectorAll('.card');
-    TweenMax.fromTo(cards, 0.5, { y: 100, opacity: 0 }, { y: 0, opacity: 1, delay: 0.5 });
+    const card = document.querySelectorAll('.card');
+    TweenMax.fromTo(card, 0.5, { y: 100, opacity: 0 }, { y: 0, opacity: 1, delay: 0.5 });
   }, []);
 
   return (
     <div className="card">
-      <img className="ab" src={image} alt="Card Image" />
+      <img className="ab" src={image} alt="Card_Image" />
       <h2 className="title">{name}</h2>
       <p className="des">{description}</p>
       <div className="btn2">
@@ -48,8 +44,8 @@ const Card = ({ name, description, image, review, price, time }) => {
         </button>
       </div>
       <div className="extra">
-        <p>Price: {price}💰</p>
-        <p>Reviews: {review}⭐</p>
+        <p>Price:-{price}💰</p>
+        <p>Reviews:-{review}⭐</p>
         <p>{time}min</p>
       </div>
     </div>
